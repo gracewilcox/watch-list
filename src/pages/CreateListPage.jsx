d648e63317a0ac1266040a7c2dc3329a98ef95b4
gracewilcox <graceawilcox@gmail.com>
2021-01-30 15:31:27 -0500

    make Create List Page

--- src/pages/CreateListPage.jsx
+++ src/pages/CreateListPage.jsx
@@ -1,15 +1,37 @@
-import { PageContent, Heading, InputField } from 'bumbag'
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik'
+import { Box, Button, PageContent, Stack, Heading, Paragraph, InputField, FieldStack } from 'bumbag'
 
 const CreateListPage = () => {
-    
-    return (
-      <PageContent>
-          <Heading>
-              Create Movie List!
-          </Heading>
-          <InputField label="Name of your list" placeholder="Enter list name here ..." />
-      </PageContent>
-    );
-  }
+  const [loading, setLoading] = useState(false);
+  return (
+    <PageContent>
+      <Stack>
+        <Heading>Create Movie List</Heading>
+        <Paragraph>Create your own personal movie list here</Paragraph>
+        <Formik
+          initialValues={{ listName: ''}}
+          onSubmit={async (data) => {
+            setLoading(true);
+            console.log(data);
+            setLoading(false);
+          }}
+        >
+          <Form>
+            <FieldStack>
+              <Field component={InputField.Formik} name='listName' label='listName' />
+              <Box alignX='right'>
+                <Button isLoading={loading} type='submit' palette='primary'>
+                  Create List
+                </Button>
+              </Box>
+            </FieldStack>
+          </Form>
+        </Formik>
+      </Stack>
+    </PageContent>
+  );
   
-  export default CreateListPage;
+}
+  
+ export default CreateListPage;
